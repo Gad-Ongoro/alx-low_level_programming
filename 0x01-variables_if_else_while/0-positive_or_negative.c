ed02d65fa5e85706aa71077bf14b41f80286f1ee
Gad-ongoro <gadongoro1@gmail.com>
2023-06-16 05:26:20 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - print whether the number stored in the variable n is positive, negative, or zero.
+ * main - prints number stored in the variable n is positive, negative, or 0
  *
  * Return: Always 0 (Success)
  */
