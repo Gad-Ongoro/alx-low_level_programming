a1c621d28ac0233be4f1faca7803d91032e59649
Gad-ongoro <gadongoro1@gmail.com>
2023-06-20 05:48:50 -0700

    prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,12 +4,9 @@
  * print_sign - prints the sign of a number
  *
  * @n:  integer to be checked
- *
  * Return: Returns 1 and prints + if n is greater than zero
- *
- * Returns 0 and prints 0 if n is zero
- *
- * Returns  -1 and prints - if n is less than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
