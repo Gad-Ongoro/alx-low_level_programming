8d7e1c54330749ab90d1d59a8fe970485780eb19
Gad-ongoro <gadongoro1@gmail.com>
2023-07-17 07:24:24 -0700

    C - Structures, typedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,104 @@
+#include "dog.h"
+
+#include <stdlib.h>
+
+/**
+ * strLen - returns string length
+ *
+ * @string: string to evaluate
+ *
+ * Return: SUCCESS
+ */
+
+int strLen(char *string)
+{
+	int myInt1 = 0;
+
+	while (string[myInt1] != '\0')
+	{
+		myInt1++;
+	}
+
+	return (myInt1);
+}
+
+/**
+ * *strCopy - copies the string pointed to by src
+ *
+ * including the terminating null byte (\0)
+ *
+ * to the buffer pointed to by dest
+ *
+ * @dest: destination
+ *
+ * @src: source
+ *
+ * Return: SUCCESS
+ *
+ */
+
+char *strCopy(char *dest, char *src)
+{
+	int length, myInt1;
+
+	length = 0;
+
+	while (src[length] != '\0')
+	{
+		length++;
+	}
+
+	for (myInt1 = 0; myInt1 < length; myInt1++)
+	{
+		dest[myInt1] = src[myInt1];
+	}
+	dest[myInt1] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - a function that creates a new dog.
+ *
+ * @name: name
+ *
+ * @age: age
+ *
+ * @owner: owner
+ *
+ * Return: SUCCESS
+ *
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+*dog;
+	int length1;
+	int length2;
+
+	length1 = strLen(name);
+	length2 = strLen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (length1 + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (length2 + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	strCopy(dog->name, name);
+	strCopy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,16 +20,21 @@ struct dog
 };
 
 /**
- * dogTdf - typedef for dog
+ * dog_t - typedef for dog
  *
  */
 typedef struct dog dogTdf;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
 void print_dog(struct dog *d);
-dogTdf *new_dog(char *name, float age, char *owner);
-void free_dog(dogTdf *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
 char *_strcpy(char *dest, char *src);
+
 int _strlen(char *s);
 
 #endif
