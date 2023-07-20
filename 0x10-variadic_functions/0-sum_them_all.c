f4867e8e89a87a002a1cf9bd5bba6cb46fd21676
Gad-ongoro <gadongoro1@gmail.com>
2023-07-19 22:40:41 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -21,7 +21,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(myList, n);
 
 	for (myInt1 = 0; myInt1 < n; myInt1++)
-		sum += va_arg(myList, int);
+		result += va_arg(myList, int);
 
 	va_end(myList);
 
